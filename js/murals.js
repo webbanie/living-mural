d76e00c22b6d83e284e6aa8d12a698367fb124f7
webbanie <36488667+webbanie@users.noreply.github.com>
2019-10-21 22:31:19 -0400

    Add files via upload

---
+++ js/murals.js
@@ -0,0 +1,61 @@
+var items = [
+      { id: 0, title: 'natives beauties', city: 'toronto', country: 'Canada'},
+      { id: 1, title: 'the view', city: 'toronto', country: 'Canada'},
+      { id: 2, title: 'this is paradise', city: 'toronto', country: 'Canada'},
+      { id: 3, title: 'in progress 01', city: 'toronto', country: 'Canada'},
+      { id: 4, title: 'in progress 02', city: 'toronto', country: 'Canada'},
+      { id: 5, title: 'the stair view', city: 'toronto', country: 'Canada'},
+      { id: 6, title: 'abstract Horses', city: 'toronto', country: 'Canada'},
+      { id: 7, title: 'rainbow thinker', city: 'toronto', country: 'Canada'},
+      { id: 8, title: 'blue hair glam', city: 'toronto', country: 'Canada'},
+      { id: 9, title: 'a memory', city: 'toronto', country: 'Canada'},
+      { id: 10, title: 'look away from the ocean', city: 'toronto', country: 'Canada'},
+      { id: 11, title: 'king fishy', city: 'tor', country: 'Canada'},
+      { id: 12, title: 'a nature song', city: 'toronto', country: 'Canada'},
+      { id: 13, title: 'es la vida', city: 'toronto', country: 'Canada'},
+];
+
+for (var i = 0; i <= 13; i++) {
+		//console.log(items[i])
+	};
+
+$( "#canada" ).click(function() {
+	$( ".tor" ).style.display = "none";
+	$( ".toronto" ).css( "border", "13px solid red" );
+});
+
+//******************DATA
+
+var murals = new Vue({
+  el: '#murals',
+  data: {
+    items
+  },
+	methods: {	
+		itemClicked: function(item) {
+        	this.title = item.title;
+        	this.id = item.id;
+			//$("#myModal").modal('show');
+
+			// Get the modal
+			var modal = document.getElementById("myModal");
+
+			// Get the image and insert it inside the modal - use its "alt" text as a caption
+			var img = document.getElementById(this.id);
+			var modalImg = document.getElementById("img01");
+			var captionText = document.getElementById("caption");
+			
+			modal.style.display = "block";
+			modalImg.src = './img/mural_to' + this.id + '.jpg';
+			captionText.innerHTML = this.title;
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+        		modal.style.display = "none";
+			}
+        }
+    }
+});
---
+++ js/portraits.js
@@ -0,0 +1,37 @@
+var portraits = [
+	{ 	no: "1", img: "308x560_1", title: "Our streets beauties", text1: "Across many cities, Street Murals and Graffitis live and make us travel to other countries, other landscapes. ", text2: "Sometimes conveying messages or giving way to the abstract, the beauty of this art follows us at every street corner.", style: "background-color: #198f75;", href: "murals.html", view: "MURALS"},
+	{ 	no: "2", img: "308x560_2", title: "The Art in Motion", text1: "Static and full of meaning, the paintings as varied as they are, live and from the mind of the viewers, emanate a melody and let us imagine movements thus giving life to the masterpiece. ", text2: "Let yourself be guided by some personal animated interpretations of these works.", style: "background-color: #478c40;", href: "animations.html", view: "VIDEOS"},
+	{ 	no: "3", img: "308x560_3", title: "Framed Creativity", text1: "Across many cities, Street Murals and Graffitis live and make us travel to other countries, other landscapes. ", text2: "Sometimes conveying messages or giving way to the abstract, the beauty of this art follows us at every street corner.", style: "background-color: #b6b10d;", href: "murals.html", view: "MURALS"},
+];
+
+//***********PORTRAITS
+var portraits = new Vue({
+  el: '#portraits',
+  data: {
+    portraits
+  },
+});
+
+/**/$('.portrait').on('click', function () {
+    $('.portrait').not(this).toggle("drop");
+    $('.content').toggle("drop");
+
+    var $parentDiv = $(this).parent('div');
+
+    if ($parentDiv.is('#player2-col')) {
+        $parentDiv.toggleClass("move-player2");
+    } else if ($parentDiv.is('#player3-col')) {
+        $parentDiv.toggleClass("move-player3");
+
+    } else if ($parentDiv.is('#player4-col')) {
+        $parentDiv.toggleClass("move-player4");
+
+    } else if ($parentDiv.is('#player5-col')) {
+        $parentDiv.toggleClass("move-player5");
+    }
+    
+    $('.content').html($parentDiv.find('[id^="content"]').html());
+});
+
+//******************DATA
+
